acd8cb65bede1b2ec72a9fd8e8b9d5be99a6fef7
arsproger <arsproger@gmail.com>
2022-09-27 18:52:22 +0600

    init

---
+++ src/module1/Lesson9.java
@@ -0,0 +1,11 @@
+package module1;
+
+import java.util.Scanner;
+
+public class Lesson9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
