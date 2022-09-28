82c031560f4ebb261cc67662b76e9c63003d305b
arsproger <arsproger@gmail.com>
2022-09-28 18:50:49 +0600

    init

---
+++ src/module1/Lesson10.java
@@ -0,0 +1,7 @@
+package module1;
+
+public class Lesson10 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/module1/Lesson9.java
+++ src/module1/Lesson9.java
@@ -1,10 +1,16 @@
 package module1;
 
-import java.util.Scanner;
 
 public class Lesson9 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        array[0] = 1;
+        array[9] = 10;
+        for(int num: array)
+            System.out.println(num);
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
 
 
     }
