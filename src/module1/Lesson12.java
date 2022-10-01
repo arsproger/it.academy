70c9e5d3e2b170b14c71b717b3176877d2ea970c
arsproger <arsproger@gmail.com>
2022-10-01 18:48:28 +0600

    Init

--- src/module1/Lesson12.java
+++ src/module1/Lesson12.java
@@ -1,7 +1,20 @@
 package module1;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Lesson12 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int maxElement = 0;
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+            if(array[i] > maxElement)
+                maxElement = array[i];
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Max element: " + maxElement);
 
     }
 }
---
+++ src/module1/Lesson13.java
@@ -0,0 +1,7 @@
+package module1;
+
+public class Lesson13 {
+    public static void main(String[] args) {
+
+    }
+}
